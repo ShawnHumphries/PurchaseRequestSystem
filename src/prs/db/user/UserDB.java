f53b56e2d955e824f3768b01427a0608f22c8ab4
ShawnHumphries <sahump1@gmail.com>
2017-05-16 17:10:18 -0400

    Added method to add user to DB

--- src/prs/db/user/UserDB.java
+++ src/prs/db/user/UserDB.java
@@ -1,5 +1,33 @@
 package prs.db.user;
 
+import java.sql.*;
+
+import prs.business.User;
+import prs.util.DBUtil;
+
 public class UserDB implements UserDAO {
 
+	@Override
+	public boolean addUser(User inUser) {
+		String sql = "INSERT INTO users (UserName, Password, FirstName, LastName, Phone, EMail, Manager) VALUES (?, ?, ?, ?, ?, ?, ?)";
+		Connection connection = DBUtil.getConnection();
+		try (PreparedStatement ps = connection.prepareStatement(sql))
+		{
+			ps.setString(1, inUser.getUsername());
+			ps.setString(2, inUser.getPassword());
+			ps.setString(3, inUser.getFirstName());	
+			ps.setString(4, inUser.getLastName());
+			ps.setString(5, inUser.getPhone());
+			ps.setString(6, inUser.getEmail());
+			ps.setBoolean(7, inUser.isManager());
+			ps.executeUpdate();
+		}
+		catch (SQLException e)
+		{
+			System.out.println(e);
+			return false;
+		}
+		return true;
+	}
+	
 }
--- src/prs/db/user/UserWriter.java
+++ src/prs/db/user/UserWriter.java
@@ -1,5 +1,8 @@
 package prs.db.user;
 
+import prs.business.User;
+
 public interface UserWriter {
 
+	boolean addUser(User inUser);
 }
