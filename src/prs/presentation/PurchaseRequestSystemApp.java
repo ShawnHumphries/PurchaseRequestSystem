9dd64bcb8bb62c3a5a3bc8a338b204eee9641025
ShawnHumphries <sahump1@gmail.com>
2017-05-19 21:59:29 -0400

    Added methods to display vendors and products

--- src/prs/presentation/PurchaseRequestSystemApp.java
+++ src/prs/presentation/PurchaseRequestSystemApp.java
@@ -1,12 +1,18 @@
 package prs.presentation;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.Scanner;
 
+import prs.business.Product;
 import prs.business.User;
+import prs.business.Vendor;
 import prs.db.DAOFactory;
+import prs.db.product.ProductDAO;
 import prs.db.user.UserDAO;
+import prs.db.vendor.VendorDAO;
+import prs.util.StringUtil;
 import prs.util.Validator;
 
 /*
@@ -18,6 +24,8 @@ public class PurchaseRequestSystemApp {
 
 	private static Scanner sc = new Scanner(System.in);
 	private static UserDAO userDAO = DAOFactory.getUserDAO();
+	private static VendorDAO vendorDAO = DAOFactory.getVendorDAO();
+	private static ProductDAO productDAO = DAOFactory.getProductDAO();
 
 	public static void main(String[] args) {
 
@@ -39,6 +47,17 @@ public class PurchaseRequestSystemApp {
 			System.out.println();
 			// Perform the command 
 			switch (command) {
+			case "add":
+			case "register":
+				registerNewUser();
+				break;
+			case "request":
+			case "review":
+			case "vendor":
+				displayAllVendors();
+				break;
+			case "product":
+				displayProductsByVendor();
 			case "help":
 				break;
 			case "exit":
@@ -52,8 +71,8 @@ public class PurchaseRequestSystemApp {
 	 */
 	private static void displayMenu() {
 		System.out.println("COMMAND MENU");
-		System.out.println("add\t- Add a user");
-		System.out.println("req\t- Create a request");
+		System.out.println("register\t- Register a user");
+		System.out.println("request\t- Create a request");
 		System.out.println("review\t- Review submitted requests");
 		System.out.println("vendor\t- Display vendor info");
 		System.out.println("product\t- Display product info per vendor");
@@ -70,9 +89,8 @@ public class PurchaseRequestSystemApp {
 			// Make sure the command the user entered is valid
 			switch (command) {
 			case "add":
-				addNewUser();
-				break;
-			case "req":
+			case "register":
+			case "request":
 			case "review":
 			case "vendor":
 			case "product":
@@ -89,12 +107,12 @@ public class PurchaseRequestSystemApp {
 		return command;
 	}
 
-	private static void addNewUser() {
+	private static void registerNewUser() {
 		String userID = Validator.getString(sc, "Enter a user id: ");
 		String password = Validator.getString(sc, "Enter password: ");
 		String firstName = Validator.getString(sc, "Enter first name: ");
 		String lastName = Validator.getString(sc, "Enter last name: ");
-		String phoneNumber = Validator.getString(sc, "Enter phone number (xxxyyyzzzz)");
+		String phoneNumber = Validator.getString(sc, "Enter phone number (xxxyyyzzzz): ");
 		String email = Validator.getString(sc, "Enter email: ");
 		String manager = Validator.getString(sc, "Is the user a manager (y/n) ");
 		boolean isManager = false;
@@ -105,6 +123,45 @@ public class PurchaseRequestSystemApp {
 			System.out.println("\nUser ID " + userID + " has been added.\n");
 		else
 			System.out.println("\nUser ID " + userID + " was not added successfully.\n");
-		
 	}
+	
+	private static void displayAllVendors() {
+		
+		ArrayList<Vendor> vendors = vendorDAO.getAllVendors();
+		if (vendors.size() > 0)
+		{
+			System.out.println("Code\t\tName\t\t\t\tAddress\t\t\t\tCity\t\tState\tZip\tPhone\t\tEMail\t\tPreApproved?");
+			for (Vendor vendor : vendors)
+			{
+				System.out.print(vendor.getCode() + "\t" + vendor.getName() + "\t" + vendor.getAddress() + "\t" + vendor.getCity() + "\t" + vendor.getState() + "\t" + vendor.getZip());
+				System.out.print("\t" + vendor.getPhone() + "\t" + vendor.getEmail() + "\t");
+				if (vendor.isPreapproved())
+					System.out.println("Yes");
+				else
+					System.out.println("No");
+			}
+		}
+		else
+			System.out.println("There are no vendors to purchase products from.");
+		System.out.println();
+
+	}
+	
+	private static void displayProductsByVendor() {
+		
+		ArrayList<Product> productsByVendor = productDAO.getProductsByVendorID(2);
+		if (productsByVendor.size() > 0)
+		{
+			System.out.println("Name\t\t\t\tPartNumber\tPrice\tUnit\tPhotoPath");
+			for (Product product : productsByVendor)
+			{
+				System.out.print(product.getName() + "\t" + product.getPartNumber() + "\t" + StringUtil.getFormattedDouble(product.getPrice()));
+				System.out.println("\t" + product.getUnit() + "\t" + product.getPhotoPath());
+			}
+		}
+		else
+			System.out.println("No products found for this vendor.");
+		System.out.println();
+	}
+	
 }
