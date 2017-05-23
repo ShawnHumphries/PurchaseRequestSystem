5ae55fbeb5e4f8b4c477dcbf5dea6c7d586e911c
ShawnHumphries <sahump1@gmail.com>
2017-05-22 23:34:36 -0400

    Added methods to convert Strings to SQL Date objects

--- src/prs/util/StringUtil.java
+++ src/prs/util/StringUtil.java
@@ -1,6 +1,9 @@
 package prs.util;
 
+import java.sql.Date;
 import java.text.NumberFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 /*
  * The StringUtil class contains utility methods to help with strings.
@@ -17,4 +20,33 @@ public class StringUtil {
         return currency.format(input);
 	}
 
+	/*
+	 * Converts today's date into a java.sql.Date object formatted as "yyyy-MM-dd"
+	 */
+	public static Date convertTodaysDateToSQLDate()
+	{
+		java.util.Date today = new java.util.Date();
+		return new Date(today.getTime());
+	}
+	
+	/*
+	 * Converts a string formatted as "yyyy-MM-dd" to a java.sql.Date object
+	 */
+	public static Date convertStringToSQLDate(String dateString)
+	{
+		java.util.Date tempDate;
+
+		try 
+		{
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+			tempDate = sdf.parse(dateString);
+			return new Date(tempDate.getTime());
+		} 
+		catch (ParseException e)
+		{
+			System.out.println(e);
+			return null;
+		}
+	}
+	
 }
