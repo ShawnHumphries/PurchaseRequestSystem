3d3e7b80fde9e6b90e35dc4e6506df4c54dab191
ShawnHumphries <sahump1@gmail.com>
2017-05-24 17:23:27 -0400

    Added method to get LineItem by request ID

--- src/prs/db/lineitem/LineItemDB.java
+++ src/prs/db/lineitem/LineItemDB.java
@@ -2,13 +2,18 @@ package prs.db.lineitem;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import prs.business.LineItem;
+import prs.business.Request;
 import prs.util.DBUtil;
 
 public class LineItemDB implements LineItemDAO {
 
+	ArrayList<LineItem> lineItems = null;
+	
 	@Override
 	public boolean addLineItem(LineItem inLineItem) {
 
@@ -29,4 +34,42 @@ public class LineItemDB implements LineItemDAO {
 		return true;
 	}
 
+	@Override
+	public ArrayList<LineItem> getLineItemsByRequestID(int inRequestID) {
+
+		lineItems = new ArrayList<>();
+		String sql = "SELECT ID, RequestID, ProductID, Quantity "
+				+ "from lineitems "
+				+ "where RequestID = ? "
+				+ "order by ID";
+        Connection connection = DBUtil.getConnection();
+        try (PreparedStatement ps = connection.prepareStatement(sql))
+        {
+        	ps.setInt(1, inRequestID);
+            ResultSet rs = ps.executeQuery();
+        	while (rs.next())
+        	{
+        		LineItem li = getLineItemFromRow(rs);
+        		lineItems.add(li);
+        	}
+        }
+        catch (SQLException e)
+        {
+        	System.out.println(e);
+        	return null;
+        }
+		return lineItems;
+	}
+
+	private static LineItem getLineItemFromRow(ResultSet rs) throws SQLException {
+		
+		LineItem lineItem = new LineItem();
+		
+		lineItem.setId(rs.getInt("ID"));
+		lineItem.setRequestID(rs.getInt("RequestID"));
+		lineItem.setProductID(rs.getInt("ProductID"));
+		lineItem.setQuantity(rs.getInt("Quantity"));
+		
+		return lineItem;
+	}
 }
